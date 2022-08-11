fdf4f5ad810dc603827aa45be28c9b94e19590ee
Macbook <macbook@Macbooks-MacBook-Pro.local>
2022-08-11 08:15:12 +0400

    Adenza Task

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/** @format */
+
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function () {
+  document.querySelector("body").classList.toggle("active");
+});
